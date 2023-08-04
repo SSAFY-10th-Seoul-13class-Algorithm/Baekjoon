682a7c620ae36e9e7b7c527ee56a3a644674ad55
heeyeon3050 <kho200a@naver.com>
2023-08-04 22:38:56 +0900

    파일 수정

--- src/main/java/org/example/week_01/BOJ_11403_경로찾기_김희연.java
+++ src/main/java/org/example/week_01/BOJ_11403_경로찾기_김희연.java
@@ -5,7 +5,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-public class BOJ_S1_11403_경로찾기_김희연 {
+public class BOJ_11403_경로찾기_김희연 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
